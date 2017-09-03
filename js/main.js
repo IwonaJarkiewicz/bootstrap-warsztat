bf500c8e2b9e8579ff511c0d38abc2c17a880e9d
IwonaJarkiewicz <politea@gmail.com>
2017-09-03 14:22:11 +0200

    portfolio

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
 'use strict'
 $(document).ready(function () {
-    $('#carousel-example-generic').carousel({
-        interval: 1000,
-        pause: "hover",
-        wrap: false
-    });
+    navAddClass();
 });
+
+$(window).scroll(function () {
+    navAddClass();
+});
+
+function navAddClass() {
+    var navHeight = $('#main-nav').height();
+    console.log(navHeight);
+
+    var windowScrollPosition = $(window).scrollTop();
+    consol.log(windowScrollPosition);
+    if (windowScrollPosition >= navHeight) {
+        $('#main-nav').addClass('scrolled');
+    } else {
+        $('navHeight').removeClass('scrolled');
+    }
+}
